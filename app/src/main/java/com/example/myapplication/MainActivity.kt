dc4354310ee2c5e482702b2ffe8691a362e5182f
seongwon-bae <bsy8854@naver.com>
2021-05-23 15:40:36 +0900

    Remake OrderCheckActivity.kt & activity_order_check.xml Connect activity_main.xml & activity_order_check.xml

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View;
-import android.widget.Button
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val nextIntent = Intent(this, OrderCheckActivity::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
