5a765efa6c1078aa054b6e64dc0a6177ee952d9b
tlsalsckd13 <tlsalsckd13@naver.com>
2021-06-11 11:52:25 +0900

    store_id table_num 데이터 값 HomeActivity.kt -> MainActivity.kt 이동

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(intent.getStringExtra("store_num"))
+        println(intent.getStringExtra("table_num"))
+        println("===================store_num : ${intent.getStringExtra("store_num")}=======================")
 
         button.setOnClickListener {
             val nextIntent = Intent(this, OrderCheckActivity::class.java)
